334d85059bcd4d96901f2afd1ffc2054932e8ed2
Fatima <fatimavaliyeva0704@gmail.com>
2024-06-11 17:41:57 +0400

    Add project files.

---
+++ 11-06-24  hw/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _11_06_24__hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
