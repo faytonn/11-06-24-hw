13a7f705c135325300268400e35340e1dbea041b
Fatima <fatimavaliyeva0704@gmail.com>
2024-06-11 18:59:59 +0400

    bugs fixed

--- 11-06-24  hw/Program.cs
+++ 11-06-24  hw/Program.cs
@@ -1,10 +1,68 @@
-﻿namespace _11_06_24__hw
+﻿using _11_06_24__hw;
+using System.Data;
+
+User[] users = new User[3];
+bool systemProcess = true;
+
+for (int i = 0; i < users.Length; i++)
 {
-    internal class Program
+    Console.Write("Enter full name: ");
+    string fullName = Console.ReadLine();
+
+    Console.Write("Enter email: ");
+    string email = Console.ReadLine();
+
+    while (true)
     {
-        static void Main(string[] args)
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+
+        User temporaryUser = new User(fullName, email, password);
+        if (temporaryUser.PasswordChecker(password))
         {
-            Console.WriteLine("Hello, World!");
+            users[i] = temporaryUser;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid password written. \n(At least 1 digit & capital and lowercase letter.");
+
+        }
+    }
+
+    while (systemProcess)
+    {
+        Console.WriteLine("Commands:");
+        Console.WriteLine("1. Show all users     OR     /show");
+        Console.WriteLine("2. Get info by ID     OR     /info");
+        Console.WriteLine("0. Quit               OR     /exit");
+
+        Console.Write("Select a command: ");
+        string command = Console.ReadLine();
+
+        if (command == "1" || command == "/show")
+        {
+            foreach (User user in users)
+            {
+                user.GetInfo();
+            }
+        }
+
+        else if (command == "2" || command == "/info")
+        {
+            Console.Write("Enter user ID: ");
+            int _id = int.Parse(Console.ReadLine());
+
+           string result = User.FindUserByID(_id, users);
+        }
+        else if (command == "3" || command == "/exit") 
+        {
+            systemProcess = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Please write a valid command");
         }
     }
 }
---
+++ 11-06-24  hw/User.cs
@@ -0,0 +1,86 @@
+﻿namespace _11_06_24__hw
+{
+    public class User
+    {
+        int i = 0;
+        private static int _id = 0;
+        public int Id { get; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string _password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                while (true)
+                {
+
+                    if (PasswordChecker(value))
+                    {
+                        _password = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid password written. \n(At least 1 digit & capital and lowercase letter.");
+                        break;
+                    }
+                }
+            }
+
+
+        }
+        public User(string fullName, string email, string password)
+        {
+            _id++;
+            Id = _id;
+            FullName = fullName;
+            Email = email;
+            _password = password;
+        }
+
+        public bool PasswordChecker(string password)
+        {
+            bool hasUpper = false;
+            bool hasLower = false;
+            bool hasDigit = false;
+
+            if (password.Length < 8)
+            {
+                return false;
+            }
+
+            foreach (char c in password)
+            {
+                if (char.IsUpper(c)) hasUpper = true;
+                if (char.IsLower(c)) hasLower = true;
+                if (char.IsDigit(c)) hasDigit = true;
+            }
+
+            return hasUpper && hasLower && hasDigit;
+        }
+
+        public string GetInfo()
+        {
+            return $"ID: {Id} \nFullName: {FullName} \nEmail: {Email}";
+        }
+
+        public static string FindUserByID(int id, User[] users)
+        {
+            foreach (User user in users)
+            {
+                if (user.Id == id)
+                {
+                    return user.GetInfo();
+                }
+
+            }
+
+            return "User is not found";
+
+        }
+
+    }
+}
